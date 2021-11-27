6f3b8c9b6cbfee3722319760158e7144239db651
DiorChoppa <78878912+DiorChoppa@users.noreply.github.com>
2021-11-27 18:28:28 +0300

    Delete modules directory

--- modules/main.py
+++
@@ -1,6 +0,0 @@
-import my_module
-print(my_module.s)
-my_module.a
-my_module.foo(['1', '2', '3'])
-x = my_module.Foo()
-x
--- modules/my_module.py
+++
@@ -1,7 +0,0 @@
-#my_module.py
-s = "This is Python course"
-a = [100, 200, 300]
-def foo(arg):
-    print(arg)
-class Foo():
-    pass
